5ca99005b9f424c9881633a3a678de4681cb836a
jorgelavallez-create <jorgelavallez@gmail.com>
2026-05-24 17:39:44 -0600

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -16,11 +16,11 @@ module.exports = async function handler(req, res) {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ 
-  contents: [{ parts }],
-  generationConfig: {
-    maxOutputTokens: 2048
-  }
-})
+          contents: [{ parts }],
+          generationConfig: { maxOutputTokens: 2048 }
+        })
+      }
+    );
     const data = await response.json();
     const text = data.candidates?.[0]?.content?.parts?.[0]?.text || "";
     res.status(200).json({ text });
